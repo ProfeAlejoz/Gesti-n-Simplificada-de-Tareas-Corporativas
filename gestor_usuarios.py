68451547a9017396b01255d1efc13cead0240eec
Ludeña Quichimbo Edison Wilfrido <132524997+edison32wlq@users.noreply.github.com>
2024-12-04 12:02:03 -0500

    funcion registro 1.1

--- gestor_usuarios.py
+++ gestor_usuarios.py
@@ -6,34 +6,40 @@ print("Por favor, regístrate\n")
 
 def registro():
     while True:
-        nombre = input("Nombre: ")
-        if nombre.strip() == "":
-            print("El nombre no puede estar vacío. Por favor ingresa un nombre válido.")
-        else:
-            break
-    
-    while True:
-        try:
-            edad = int(input("Ingrese su edad: "))
-            if edad <= 0:
-                print("La edad debe ser real.")
+        while True:
+            nombre = input("Nombre: ")
+            if nombre.strip() == "":
+                print("El nombre no puede estar vacío. Por favor ingresa un nombre válido.")
             else:
                 break
-        except ValueError:
-            print("Por favor ingresa un número válido para la edad.")
-    
-    while True:
-        correo = input("Ingrese su correo: ")
-        if not re.match(r"[^@]+@[^@]+\.[^@]+", correo):
-            print("El correo ingresado no es válido. Asegúrese de que contenga '@' y un dominio.")
-        else:
+        
+        while True:
+            try:
+                edad = int(input("Ingrese su edad: "))
+                if edad <= 0:
+                    print("La edad debe ser un número positivo.")
+                else:
+                    break
+            except ValueError:
+                print("Por favor ingresa un número válido para la edad.")
+        
+        while True:
+            correo = input("Ingrese su correo: ")
+            if not re.match(r"[^@]+@[^@]+\.[^@]+", correo):
+                print("El correo ingresado no es válido. Asegúrese de que contenga '@' y un dominio.")
+            else:
+                break
+
+        usuario = {
+            "nombre": nombre,
+            "edad": edad,
+            "correo": correo,
+        }
+        usuarios.append(usuario)
+        print(f"Usuario {nombre} registrado correctamente.\n")
+        
+        continuar = input("¿Deseas registrar otro usuario? (s/n): ")
+        if continuar.lower() != 's':
             break
-    
-    usuario = {
-        "nombre": nombre,
-        "edad": edad,
-        "correo": correo,
-    }
-    usuarios.append(usuario)
-    print(f"Usuario {nombre} registrado correctamente.\n")
+
 registro()
